16b824813b2c29dfb91f35f26ca16a12bffec263
Gottfried Chen <gottfried.chen@allaboutapps.at>
2024-08-01 17:10:54 +0200

    add commit hash to header

--- src/components/providers/DebugProvider.tsx
+++ src/components/providers/DebugProvider.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useDebugCommands } from "@/hooks/useDebugCommands";
+import { useEffect } from "react";
 
 export const DebugProvider = ({ children }: { children: React.ReactNode }) => {
     useDebugCommands();
+
+    // Add commit hash as header comment for deployed versions
+    useEffect(() => {
+        const version = document.createComment(`Version ${process.env.NEXT_PUBLIC_COMMIT_HASH}`);
+        document.head.prepend(version);
+    }, []);
 
     return <>{children}</>;
 };
