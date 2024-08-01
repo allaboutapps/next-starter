791f804336bfddd904bf9fec09cb13d40fd4c8e0
Gottfried Chen <gottfried.chen@allaboutapps.at>
2024-08-01 11:18:11 +0200

    fix SSR error

--- src/hooks/useDebugCommands.ts
+++ src/hooks/useDebugCommands.ts
@@ -1,9 +1,14 @@
+import { IS_SERVER } from "@/config";
 import { useDebugStore } from "../stores/debugStore";
 
 // Hook that binds various debug commands to the window object
 // Only use this in your top-level component e.g. App.tsx
 export function useDebugCommands() {
     const enableDebug = useDebugStore((state) => state.enable);
+
+    if (IS_SERVER) {
+        return;
+    }
 
     // Bind debug commands to window object
     // Use syntax debugXyz() when adding new commands
